94ca1d0655a28a9484b2dfe20031361262a9d561
Zadkiel Troche <troche20001@gmail.com>
2023-06-22 10:47:45 -0400

    Primer commit

---
+++ src/main/java/com/comercio/comElectronico/ComercioElectronicoApplication.java
@@ -0,0 +1,13 @@
+package com.comercio.comElectronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioElectronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioElectronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comercio/comElectronico/ComercioElectronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comercio.comElectronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioElectronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
